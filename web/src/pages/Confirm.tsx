f70162be1b6d7df691850d777c22226d7f0d7f90
pablo <devpabloh@gmail.com>
2025-06-11 22:00:21 -0300

    criando e estilizando o componente de confirm

--- web/src/pages/Confirm.tsx
+++ web/src/pages/Confirm.tsx
@@ -1,4 +1,5 @@
 import { Navigate, useLocation } from "react-router"
+import okSvg from "../assets/ok.svg";
 
 export function Confirm(){
     const location = useLocation();
@@ -8,8 +9,12 @@ export function Confirm(){
     }
 
     return(
-        <>
-            <h1>confirm</h1>
-        </>
+        <div className= "bg-gray-500 lg:w-[512px]">
+            <h1>Solicitação enviada! </h1>
+            <img src={okSvg} alt="ícone de ok" className="w-28"/>
+            <p> Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o setor financeiro irá entrar em contato com você.</p>
+
+            <a href="/">Nova solicitação </a>
+        </div>
     )
 }
