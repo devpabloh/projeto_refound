f0a98a534912ce95b8598f6d45ebeeda278036e7
pablo <devpabloh@gmail.com>
2025-06-12 23:30:07 -0300

    Instalando as dependências do Class-Merge e o clsx para criar o utils que vai fazer com que possamos usar mais de um valor no mesmo atributo.

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,9 +1,23 @@
+import { classMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean;
+    variant?: "base" | "icon" | "iconSmall";
 }
 
-export function Button({isLoading, children, type="button", ...rest}:Props){
+const variants = {
+    button: {
+        base: "h-12",
+        icon: "h-12 w-12",
+        iconSmall: "h-10 w-10",
+    }
+}
+
+export function Button({isLoading, children, type="button",variant="base", className, ...rest}:Props){
     return(
-        <button type={type} disabled={isLoading} {...rest} className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed" >{children}</button>
+        <button type={type} disabled={isLoading} {...rest} className={classMerge([
+            "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed",
+            variants.button[variant], className,
+        ])} >{children}</button>
     )
 }
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,5 +1,8 @@
+import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 import { useState } from "react"
+
+import searchSvg from "../assets/search.svg"
 
 export function Dashboard(){
     const [ name, setName] = useState("")
@@ -16,6 +19,9 @@ export function Dashboard(){
             <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
             <form className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 mf:flex-row gap-2 mt-6" onSubmit={fetchRefound}>
                 <Input placeholder="Pesquisar pelo nome" onChange={(e) => setName(e.target.value)}/>
+                <Button variant="iconSmall" >
+                    <img src={searchSvg} alt="Botão de pesquisa" className="w-5"/>
+                </Button>
             </form>
         </div>
     )
