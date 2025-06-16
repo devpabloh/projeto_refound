84809e2c73b1249e4481537ea8df8c2ba564e438
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-06-16 10:57:49 -0300

    utilizando a abordagem de action para lidar com informações do formulário

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,22 +1,24 @@
-import { useState } from "react"
+import { useActionState } from "react"
+
 import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 
 export function SignIn(){
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
+    const [state, formAction, isLoading] = useActionState(signIn, {email: "", password: ""})
 
-    function onSubmit(e: React.FormEvent){
-        e.preventDefault()
+    function signIn(prevState: any, formData: FormData){
+        
+        const email = formData.get("email")
+        const password = formData.get("password")
 
-        console.log(email, password)
+        return {email, password}
+        
     }
 
     return(
-        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-            <Input required legend="E-mail" type="email" placeholder="seuemail@email.com" onChange={(e)=> setEmail(e.target.value)}/>
-            <Input required legend="senha" type="password" placeholder="Digite sua senha..." onChange={(e)=> setPassword(e.target.value)}/>
+        <form action={formAction} className="w-full flex flex-col gap-4">
+            <Input name="email" required legend="E-mail" type="email" placeholder="seuemail@email.com" />
+            <Input name="password" required legend="senha" type="password" placeholder="Digite sua senha..."  />
             <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
             <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar Conta</a>
