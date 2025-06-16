72726e61a981011a6d6397198088c9d0ab301a83
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-06-16 09:15:20 -0300

    Criando usuário para testar o signUp

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import { z,ZodError } from "zod"
+import { api } from "../services/api"
+import { useNavigate } from "react-router"
+import { AxiosError } from "axios"
 
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
@@ -23,7 +26,9 @@ export function SignUp(){
     const [confirmPassword, setConfirmPassword] = useState("")
     const [isLoading, setIsLoading] = useState(false)
 
-    function onSubmit(e: React.FormEvent){
+    const navigate = useNavigate()
+
+    async function onSubmit(e: React.FormEvent){
         e.preventDefault()
 
         try {
@@ -36,10 +41,22 @@ export function SignUp(){
                 confirmPassword
             })
 
+            await api.post("/users", data)
+
+            if(confirm("Cadastrado com sucesso! Deseja fazer login agora?")){
+                navigate("/")
+            }
+
 
         } catch (error) {
+            console.log(error)
+
             if(error instanceof ZodError){
                 return alert(error.issues[0].message)
+            }
+
+            if(error instanceof AxiosError){
+                return alert(error.response?.data.message)
             }
 
             alert("Ocorreu um erro ao cadastrar o usuário. Tente novamente mais tarde.")
